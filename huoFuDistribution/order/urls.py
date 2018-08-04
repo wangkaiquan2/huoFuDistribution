3786cae5c65f1cb17608b5c912b9adae83777439
wangkaiquan <hope_courage@163.com>
2018-08-04 17:53:37 +0800

    inquire_order

--- huoFuDistribution/order/urls.py
+++ huoFuDistribution/order/urls.py
@@ -5,4 +5,5 @@ from order import views
 urlpatterns = [
     url(r'^test/',views.test,name='test'),
     url(r'^add-order/',views.add_order,name='add-order'),
+    url(r'^inquire-order/',views.inquire_order,name='inquire-order'),
 ]
--- huoFuDistribution/order/views.py
+++ huoFuDistribution/order/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render,HttpResponse
+import datetime
+
+from django.shortcuts import render, HttpResponse
 from django.db import DatabaseError
 import logging
 import json
@@ -38,12 +40,12 @@ def add_order(request):
     volume = float(request.POST['volume'])
     city = request.POST['city']
     address = request.POST['address']
-    remarks = request.POST.get('remarks','')
+    remarks = request.POST.get('remarks', '')
     consignee = request.POST['consignee']
     tel = request.POST['tel']
-    new_order = models.Order(company_id=company,user_id=user,order_number=order_number,shipper=shipper,
-                                quantity=quantity,weight=weight,volume=volume,city=city,address=address,
-                                remarks=remarks,consignee=consignee,tel=tel)
+    new_order = models.Order(company_id=company, user_id=user, order_number=order_number, shipper=shipper,
+                             quantity=quantity, weight=weight, volume=volume, city=city, address=address,
+                             remarks=remarks, consignee=consignee, tel=tel)
     try:
         new_order.save()
     except DatabaseError as e:
@@ -51,4 +53,58 @@ def add_order(request):
         result = {'response': '提单提交失败'}
         return HttpResponse(json.dumps(result))
     result = {'response': '提单提交成功,请刷新'}
+    return HttpResponse(json.dumps(result))
+
+
+def inquire_order(request):
+    """查询订单"""
+    orders = models.Order.objects.all()
+    now = datetime.datetime.now()
+    zeroToday = now - datetime.timedelta(hours=now.hour, minutes=now.minute, seconds=now.second,
+                                         microseconds=now.microsecond)
+    lastToday = zeroToday + datetime.timedelta(hours=23, minutes=59, seconds=59)
+    stime = zeroToday
+    etime = lastToday
+    if request.GET.get('stime', '') and request.GET.get('etime', ''):
+        stime = request.GET['stime']
+        etime = request.GET['etime']
+        print('stime:',stime)
+        print('etime:',etime)
+    orders = orders.filter(ctime__gte=stime).filter(ctime__lte=etime)
+    if request.GET.get('order_number', ''):
+        orders = orders.filter(order_number=request.GET['order_number'])
+    if request.GET.get('shipper', ''):
+        orders = orders.filter(shipper=request.GET['shipper'])
+    if request.GET.get('quantity', ''):
+        orders = orders.filter(quantity=request.GET['quantity'])
+    if request.GET.get('weight', ''):
+        orders = orders.filter(weight=request.GET['weight'])
+    if request.GET.get('volume', ''):
+        orders = orders.filter(volume=request.GET['volume'])
+    if request.GET.get('city', ''):
+        orders = orders.filter(city=request.GET['city'])
+    if request.GET.get('consignee', ''):
+        orders = orders.filter(consignee=request.GET['consignee'])
+    if request.GET.get('tel', ''):
+        orders = orders.filter(tel=request.GET['tel'])
+    if request.GET.get('remarks', ''):
+        orders = orders.filter(remarks=request.GET['remarks'])
+    lorder = []
+    for order in orders:
+        lorder.append({
+            'company': order.company_id,
+            'user': order.user_id,
+            'order_number': order.order_number,
+            'shipper': order.shipper,
+            'quantity': order.quantity,
+            'weight': order.weight,
+            'volume': order.volume,
+            'city': order.city,
+            'address': order.address,
+            'ctime': str(order.ctime),
+            'remarks': order.remarks,
+            'consignee': order.consignee,
+            'tel': order.tel,
+        })
+    result = {'response': lorder}
     return HttpResponse(json.dumps(result))
